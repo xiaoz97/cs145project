a22552da6c9b482225c65a7d0d09783983b5d9dd
Xiaoz97 <zhaoxiao0331@gmail.com>
2019-11-15 17:36:29 -0800

    New version. Deleted useless codes.

--- apriori.py
+++ apriori.py
@@ -1,11 +1,13 @@
 import os,sys
 import pandas as pd
+import json
+
 # 文件夹地址
-data_folder = '/Users/chengkai/Desktop/file/learn/project/用亲和性分析方法推荐电影/dataset/ml-100k'
+data_folder = 'D:/study/Data_Mining/proj/1m'
 # 文件地址
-ratings_filename = data_folder+'/u.data'
+ratings_filename = data_folder+'/ratings.dat'
 # 给u.data 加标题行
-all_ratings = pd.read_csv(ratings_filename, delimiter="\t", header=None, names= ["UserID","MovieID","Rating","Datetime"])
+all_ratings = pd.read_csv(ratings_filename, delimiter="::", header=None, names= ["UserID","MovieID","Rating","Datetime"])
 # 改变时间显示
 all_ratings["Datetime"] = pd.to_datetime(all_ratings['Datetime'], unit='s')
 # 添加一个特征，即评分大于3的定义为喜欢，小于3的定义为不喜欢
@@ -16,6 +18,7 @@ ratings = all_ratings[all_ratings['UserID'].isin(range(200))]
 favorable_ratings = ratings[ratings["Favorable"]]
 # 统计每个用户各喜欢哪些电影，按照UserID进行分组，并遍历每个用户看过的每一部电影
 favorable_reviews_by_users = dict((k, frozenset(v.values)) for k,v in favorable_ratings.groupby("UserID")["MovieID"])
+
 # 创建一个数据框，以便了解每部电影的影迷数量
 num_favorable_by_movie = ratings[["MovieID", "Favorable"]].groupby("MovieID").sum()
 # 初始化一个字典
@@ -97,70 +100,8 @@ from operator import itemgetter
 # 对执行度进行排序，输出置信度最高的前5条规则
 sorted_confidence = sorted(rule_confidence.items(), key=itemgetter(1), reverse=True)
 
-for index in range(5):
-    print("Rule #{0}".format(index + 1))
-    (premise, conclusion) = sorted_confidence[index][0]
-    print("Rule: If a person recommends {0} they will also recommend {1}".format(premise, conclusion))
-    print(" - Confidence: {0:.3f}".format(rule_confidence[(premise, conclusion)]))
-    print("")
+filename="rules"
 
-movie_name_filename = os.path.join(data_folder, "u.item")
-movie_name_data = pd.read_csv(movie_name_filename, delimiter="|", header=None, encoding = "mac-roman")
-movie_name_data.columns = ["MovieID", "Title", "Release Date", "Video Release", "IMDB", "<UNK>", "Action", "Adventure",
-                           "Animation", "Children's", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir",
-                           "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"]
-def get_movie_name(movie_id):
-    title_object = movie_name_data[movie_name_data["MovieID"] == movie_id]["Title"]
-    title = title_object.values[0]
-    return title
-get_movie_name(4)
-for index in range(5):
-    print("Rule #{0}".format(index + 1))
-    (premise, conclusion) = sorted_confidence[index][0]
-    premise_names = ", ".join(get_movie_name(idx) for idx in premise)
-    conclusion_name = get_movie_name(conclusion)
-    print("Rule: If a person recommends {0} they will also recommend {1}".format(premise_names, conclusion_name))
-    print(" - Confidence: {0:.3f}".format(rule_confidence[(premise, conclusion)]))
-    print("")
-
-# 用于测试的数据，200条以后
-test_dataset = all_ratings[~all_ratings['UserID'].isin(range(200))]
-# 提取用户喜欢的电影
-test_favorable = test_dataset[test_dataset["Favorable"]]
-#test_not_favourable = test_dataset[~test_dataset["Favourable"]]
-# 统计每个用户各喜欢哪些电影，按照UserID进行分组，并遍历每个用户看过的每一部电影
-test_favorable_by_users = dict((k, frozenset(v.values)) for k, v in test_favorable.groupby("UserID")["MovieID"])
-#test_not_favourable_by_users = dict((k, frozenset(v.values)) for k, v in test_not_favourable.groupby("UserID")["MovieID"])
-#test_users = test_dataset["UserID"].unique()
-
-# 创建两个字典, 用来存储规则正例，和返例的次数
-correct_counts = defaultdict(int)
-incorrect_counts = defaultdict(int)
-# 遍历每个用户喜欢的电影数据
-for user, reviews in test_favorable_by_users.items():
-    # 遍历每条关联规则
-    for candidate_rule in candidate_rules:
-        premise, conclusion = candidate_rule
-        # 判断用户是否喜欢前提中的电影
-        if premise.issubset(reviews):
-            # 如果前提符合，看一下用户是否喜欢结论中的电影
-            if conclusion in reviews:
-                correct_counts[candidate_rule] += 1
-            else:
-                incorrect_counts[candidate_rule] += 1
-# 用正例次数除以前提条件出现的总次数，计算每条规则的置信度
-test_confidence = {candidate_rule: correct_counts[candidate_rule] / float(correct_counts[candidate_rule] + incorrect_counts[candidate_rule])
-                   for candidate_rule in rule_confidence}
-# 对执行度进行排序，输出置信度最高的前5条规则
-sorted_test_confidence = sorted(test_confidence.items(), key=itemgetter(1), reverse=True)
-print(sorted_test_confidence[:5])
-
-for index in range(5):
-    print("Rule #{0}".format(index + 1))
-    (premise, conclusion) = sorted_confidence[index][0]
-    premise_names = ", ".join(get_movie_name(idx) for idx in premise)
-    conclusion_name = get_movie_name(conclusion)
-    print("Rule: If a person recommends {0} they will also recommend {1}".format(premise_names, conclusion_name))
-    print(" - Train Confidence: {0:.3f}".format(rule_confidence.get((premise, conclusion), -1)))
-    print(" - Test Confidence: {0:.3f}".format(test_confidence.get((premise, conclusion), -1)))
-    print("")
+with open(filename+'.json','a') as outfile:
+    json.dump(sorted_confidence,outfile,ensure_ascii=False)
+    outfile.write('\n')
