58a93d781a50e8225d003ae74fd262adacbf9267
Xiaoz97 <zhaoxiao0331@gmail.com>
2019-11-16 13:25:25 -0800

    #new version Add validation.py.

--- validation.py
+++ validation.py
@@ -24,15 +24,20 @@ total=0
 print("start")
 
 for index, row in validate.iterrows():
+    #cnt标记prediction total为总数
     cnt=0
     total+=1
     for rules,confidence in sorted_confidence:
+        #遍历已获取的规则，如果conclusion与当前需判断的电影相同，则判断premise是否在用户已经看过的电影中
         if rules[1]==row["movieId"]:
             if rules[0].issubset(favorable_reviews_by_users[row["userId"]]):
                 cnt=1
+    #如果不符合任何conclusion以及premise，暂时产生0，1随机数
     if(cnt==0):
         cnt=np.random.randint(0, 1)
+    #与真实rating比对
     if cnt==row["rating"]:
         correct+=1
+        #每出现100个正确的输出此时准确率
         if(correct%100==0):
             print(correct/total)
