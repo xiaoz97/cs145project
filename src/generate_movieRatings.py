4f8624eb4b1de2d8f84cb05412f7791d91fc79fa
gqqnbig <gqqnb2005@gmail.com>
2019-11-16 19:03:54 -0800

    生成正确的提交文件，Public Score=0.51960
    #5

--- src/generate_movieRatings.py
+++ src/generate_movieRatings.py
@@ -2,12 +2,23 @@ import os
 import pandas as pd
 
 
-def mergeCsv(dataFolder):
-	a = pd.read_csv(os.path.join(dataFolder, "val_ratings_binary.csv"))
+def ensureMergedCsv(dataFolder):
 	b = pd.read_csv(os.path.join(dataFolder, "movies-year-genres.csv"))
-	c = pd.read_csv(os.path.join(dataFolder, "train_ratings_binary.csv"))
 
-	merged = a.merge(b, left_on='movieId', right_on='id')
-	merged.to_csv(os.path.join(dataFolder, "val_movies_ratings.csv"), index=False)
-	merged2 = c.merge(b, left_on='movieId', right_on='id')
-	merged2.to_csv(os.path.join(dataFolder, "train_movies_ratings.csv"), index=False)
+	if os.path.isfile(os.path.join(dataFolder, 'train_movies_ratings.csv')) == False:
+		c = pd.read_csv(os.path.join(dataFolder, "train_ratings_binary.csv"))
+		merged = c.merge(b, left_on='movieId', right_on='id')
+		merged.to_csv(os.path.join(dataFolder, "train_movies_ratings.csv"), index=False)
+
+	if os.path.isfile(os.path.join(dataFolder, 'val_movies_ratings.csv')) == False:
+		a = pd.read_csv(os.path.join(dataFolder, "val_ratings_binary.csv"))
+		merged = a.merge(b, left_on='movieId', right_on='id')
+		merged.to_csv(os.path.join(dataFolder, "val_movies_ratings.csv"), index=False)
+
+	if os.path.isfile(os.path.join(dataFolder, 'test_movies_ratings.csv')) == False:
+		test = pd.read_csv(os.path.join(dataFolder, "test_ratings.csv"))
+		# Some movies don't have year. They are eliminated from movies-year-genres.csv.
+		# Besides, we are not sure if there are new movies in the test set.
+		merged = test.merge(b, left_on='movieId', right_on='id', how='left')
+		merged = merged.fillna(0)
+		merged.to_csv(os.path.join(dataFolder, "test_movies_ratings.csv"), index=False)
--- src/naive_bays.py
+++ src/naive_bays.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import gc
+import numpy
 import os
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
@@ -12,7 +13,6 @@ import generate_movieRatings
 
 
 def trainModel():
-	global train_x, train_y, model
 	train_data = pd.read_csv(os.path.join(dataFolder, 'train_movies_ratings.csv'))
 	# seperate the independent and target variable on training data
 	train_x = train_data.drop(columns=['rating'], axis=1)
@@ -46,13 +46,12 @@ def predictValidation(model):
 	accuracy_test = accuracy_score(validation_y, predict_test)
 	print('accuracy_score on validation dataset : ', accuracy_test)
 
+
 dataFolder = datasetHelper.getDataset()
 
 DecisionTree.ensureMovieYearGenresFile(dataFolder, 'movies-year-genres.csv')
 
-if os.path.isfile(os.path.join(dataFolder, 'train_movies_ratings.csv')) == False or \
-		os.path.isfile(os.path.join(dataFolder, 'val_movies_ratings.csv')) == False:
-	generate_movieRatings.mergeCsv(dataFolder)
+generate_movieRatings.ensureMergedCsv(dataFolder)
 
 # shape of the dataset
 #print('Shape of training data :',train_data.shape)
@@ -65,3 +64,15 @@ print('Start fitting model.')
 model = trainModel()
 predictValidation(model)
 
+print('Running on test data...')
+test_x = pd.read_csv(os.path.join(dataFolder, 'test_movies_ratings.csv'))
+predict_test = model.predict(test_x)
+#
+# predict_test = numpy.array([1, 1, 0, 0, 0])
+
+predict_test = predict_test[:, None]
+# print(predict_test)
+indexes = numpy.arange(len(predict_test))[:, None]
+
+numpy.savetxt(os.path.join(dataFolder, 'submit.csv'), numpy.hstack((indexes, predict_test)),
+			  delimiter=',', fmt='%d', header='Id,rating', comments='')
