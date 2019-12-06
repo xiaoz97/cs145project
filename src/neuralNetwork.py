0d347a538a721f23a21ddce94c6dba4abed63714
misterbig <misterbig030@gmail.com>
2019-12-05 19:33:33 -0800

    Modified some parameters

--- src/neuralNetwork.py
+++ src/neuralNetwork.py
@@ -2,6 +2,7 @@ import pandas as pd
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
+from keras import optimizers
 import os
 import datasetHelper
 import Program
@@ -14,19 +15,21 @@ generate_movieRatings.ensureMergedCsv(dataFolder)
 
 # seperate the independent and target variable on training data
 train_data = pd.read_csv(os.path.join(dataFolder, 'train_movies_ratings.csv'))
-train_x = train_data.drop(columns=['rating'], axis=1)
-train_y = train_data['rating']
+train_x = train_data.drop(columns=['rating'], axis=1).to_numpy()
+train_y = train_data['rating'].to_numpy()
 
 # seperate the independent and target variable on testing data
 validation_data = pd.read_csv(os.path.join(dataFolder, 'val_movies_ratings.csv'))
-validation_x = validation_data.drop(columns=['rating'], axis=1)
-validation_y = validation_data['rating']
+validation_x = validation_data.drop(columns=['rating'], axis=1).to_numpy()
+validation_y = validation_data['rating'].to_numpy()
+
+# print(validation_y)
 
 # define the keras model
 model = Sequential()
 model.add(Dense(23, input_dim=23, activation='relu'))
 model.add(Dense(20, activation='relu'))
-model.add(Dense(20, activation='relu'))
+# model.add(Dense(20, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 
 # compile the keras model
