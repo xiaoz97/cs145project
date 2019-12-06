9a2f17afb4c98ee1ef6892ecfa94ef1a1b17f118
misterbig <misterbig030@gmail.com>
2019-12-06 14:03:26 -0800

    加了对参数的解释

--- src/neuralNetwork.py
+++ src/neuralNetwork.py
@@ -30,10 +30,10 @@ validation_data = pd.read_csv(os.path.join(dataFolder, 'val_movies_ratings.csv')
 validation_x = validation_data.drop(columns=['rating'], axis=1).to_numpy()
 validation_y = validation_data['rating'].to_numpy()
 
-# print(validation_y)
-
 # define the keras model
 model = Sequential()
+
+# This model has three layers: one for input layer, one hidden layer, and one output layer. Can be modified.
 model.add(Dense(10, input_dim=5, activation='relu'))
 model.add(Dense(5, activation='relu'))
 # model.add(Dense(20, activation='relu'))
@@ -43,6 +43,8 @@ model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 # fit the keras model on the dataset
+# Epoches: number of times the model goes through the entire data set
+# batch size: number of samples the model trains before updating loss 
 model.fit(train_x, train_y, epochs=5, batch_size=50)
 
 # evaluate the keras model
