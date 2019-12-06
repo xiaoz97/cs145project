8ac90720a64afdab26016ac042dc591d6a2ecadd
gqqnbig <gqqnb2005@gmail.com>
2019-12-05 23:28:13 -0800

    添加注释

--- src/DecisionTree.py
+++ src/DecisionTree.py
@@ -47,6 +47,7 @@ class Classifier(object):
 		bestClf = None
 		bestScore = -1
 		try:
+			# The ratings of user 105730 have certain patterns, that the first half of ratings are 1, and the rest are 0.
 			for train_index, test_index in sklearn.model_selection.KFold(n_splits, True, 1206).split(X, y):
 				X_train, X_test = X[train_index], X[test_index]
 				y_train, y_test = y[train_index], y[test_index]
